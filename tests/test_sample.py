0ec11ff7416c010d5ae5f8f56b9690596ac90981
japan1988 <enokitakuya1988@yahoo.co.jp>
2025-08-15 10:57:37 +0900

    test_sample.py を更新

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,3 +1,5 @@
-def test_always_passes():
-    assert True
-# CIテスト用のコメント追加
+- name: Run tests with coverage (parallel)
+  run: |
+    pip install pytest pytest-cov pytest-xdist
+    pytest -q -n auto --dist loadfile \
+      --cov=. --cov-report=xml:coverage.xml --cov-report=term
